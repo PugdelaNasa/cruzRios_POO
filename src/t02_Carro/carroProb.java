f11a9f2377006f98f0b93562c7b706ac81d08ae1
Jan Carlo Cruz Rios <pug.de.la.nasa@gmail.com>
2025-10-29 01:05:54 -0600

    tarea finalizada

--- src/t02_Carro/carroProb.java
+++ src/t02_Carro/carroProb.java
@@ -2,6 +2,6 @@ package t02_Carro;
 
 public class carroProb {
     public static void main(String[] args) {
-        
+
     }
 }
---
+++ src/t02_Geometria/t02_Geometria.java
@@ -0,0 +1,4 @@
+package t02_Geometria;
+
+public class t02_Geometria {
+}

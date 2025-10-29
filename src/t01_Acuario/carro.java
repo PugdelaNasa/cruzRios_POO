04e81be2ad7fda662efa1b12cbbc4456bf562e32
Jan Carlo Cruz Rios <pug.de.la.nasa@gmail.com>
2025-10-29 01:02:48 -0600

    tarea

---
+++ src/t01_Acuario/carro.java
@@ -0,0 +1,7 @@
+package t01_Acuario;
+
+import java.util.Scanner;
+
+public class carro {
+    static Scanner sc = new Scanner(System.in);
+}
---
+++ src/t02_Carro/carroProb.java
@@ -0,0 +1,7 @@
+package t02_Carro;
+
+public class carroProb {
+    public static void main(String[] args) {
+        
+    }
+}

3403d8d1d22b697780dc60133f8069aad6edc736
Jan Carlo Cruz Rios <pug.de.la.nasa@gmail.com>
2025-11-18 12:25:33 -0600

    Configuracion Respuesta Teclado

---
+++ src/MiJuego2D/KeyHandler.java
@@ -0,0 +1,62 @@
+package MiJuego2D;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class KeyHandler implements KeyListener {
+    public boolean leftPressed;
+    public boolean rightPressed;
+    public boolean upPressed;
+    public boolean downPressed;
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        int code = e.getKeyCode();
+
+        if (code == KeyEvent.VK_W) {
+            upPressed = true;
+        }
+
+        if (code == KeyEvent.VK_A) {
+            leftPressed = true;
+
+        }
+
+        if (code == KeyEvent.VK_S) {
+            downPressed = true;
+        }
+
+        if (code == KeyEvent.VK_D) {
+            rightPressed = true;
+        }
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        int code = e.getKeyCode();
+
+        if (code == KeyEvent.VK_W) {
+            upPressed = true;
+        }
+
+        if (code == KeyEvent.VK_A) {
+            leftPressed = true;
+
+        }
+
+        if (code == KeyEvent.VK_S) {
+            downPressed = true;
+        }
+
+        if (code == KeyEvent.VK_D) {
+            rightPressed = true;
+        }
+
+    }
+}
--- src/MiJuego2D/PanelJuego.java
+++ src/MiJuego2D/PanelJuego.java
@@ -7,20 +7,30 @@ public class PanelJuego extends JPanel implements Runnable {
     //CONFIGURACION PANTALLA
     final int originalTileSize = 16; //16x16
     final int scale = 3;
-
     final int tileSize = originalTileSize * scale; //48x48
+
     final int maxScreenCol =16; //horzional
     final int maxScreenRow=12; // vertical
-
     final int screenWidth = maxScreenCol*tileSize; //768 px
     final int screenHeight = maxScreenRow*tileSize;//576 px
 
+    //FPS
+    int FPS =60;
+
+    KeyHandler keyH = new KeyHandler();
+
     Thread gameThread;
+    //Set player s default position
+    int playerX = 100;
+    int playerY = 100;
+    int playerSpeed = 4;
 
     public PanelJuego() {
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));
         this.setBackground(Color.BLACK);
         this.setDoubleBuffered(true);
+        this.addKeyListener(keyH);
+        this.setFocusable(true);
     }
 
     public void startGameThread() {
@@ -34,9 +44,36 @@ public class PanelJuego extends JPanel implements Runnable {
         while(gameThread !=null) {
 
             // 1 ACTUALIZAR INFORMACION DE LA POSICION DEL PERSONAJE
+            update();
 
             //2 DIBUJAR EN LA PANTALLA LA ACTUALIZACION DEL PERSONAJE
+            repaint();
         }
+
+    }
+
+    public void update() {
+        if(keyH.upPressed == true) {
+            playerY -= playerSpeed;
+        }
+        else if(keyH.downPressed == true) {
+            playerY += playerSpeed;
+        }
+        else if(keyH.leftPressed == true) {
+            playerX -= playerSpeed;
+        }
+        else if(keyH.rightPressed == true) {
+            playerX += playerSpeed;
+        }
+
+    }
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Graphics2D g2 = (Graphics2D) g;
+        g2.setColor(Color.white);
+        g2.fillRect(playerX, playerY,tileSize , tileSize);
+        g2.dispose();
 
     }
 }

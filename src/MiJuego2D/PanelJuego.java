98051da79f94b19da56c3156cffb6435ff28b1cc
Jan Carlo Cruz Rios <pug.de.la.nasa@gmail.com>
2025-11-18 14:56:36 -0600

    Pug ayuda

--- src/MiJuego2D/PanelJuego.java
+++ src/MiJuego2D/PanelJuego.java
@@ -74,7 +74,7 @@ public class PanelJuego extends JPanel implements Runnable {
         g2.setColor(Color.white);
         g2.fillRect(playerX, playerY,tileSize , tileSize);
         g2.dispose();
-        System.out.println("Hola");
+        System.out.println("Hola amigos xd");
 
     }
 }
